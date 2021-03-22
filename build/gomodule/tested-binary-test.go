c4f98e4ce096d50f20f01b08443ce29f6b56c41c
Yelena <lena.skripnik.01@gmail.com>
2021-03-22 20:05:36 +0200

    Create tested-binary-test.go

---
+++ build/gomodule/tested-binary-test.go
@@ -0,0 +1,58 @@
+package gomodule
+
+import (
+    "github.com/google/blueprint"
+    "bytes"
+    "strings"
+    "testing"
+    "github.com/roman-mazur/bood"
+)
+
+func TestTestedBinFactory(t *testing.T) {
+    ctx := blueprint.NewContext()
+
+    ctx.MockFileSystem(map[string][]byte{
+        "Blueprints": []byte(`
+            go_tested_binary {
+              name: "test-out",
+              srcs: ["test-src.go", "test-src_test.go"],
+              pkg: ".",
+                testPkg: ".",
+                vendorFirst: true
+            }
+        `),
+        "test-src.go": nil,
+        "test-src_test.go": nil,
+    })
+
+    ctx.RegisterModuleType("go_tested_binary", TestedBinFactory)
+
+    cfg := bood.NewConfig()
+
+    _, errs := ctx.ParseBlueprintsFiles(".", cfg)
+    if len(errs) != 0 {
+        t.Fatalf("Syntax errors in the test blueprint file: %s", errs)
+    }
+
+    _, errs = ctx.PrepareBuildActions(cfg)
+    if len(errs) != 0 {
+        t.Errorf("Unexpected errors while preparing build actions: %s", errs)
+    }
+
+    buffer := new(bytes.Buffer)
+    if err := ctx.WriteBuildFile(buffer); err != nil {
+        t.Errorf("Error writing ninja file: %s", err)
+    } else {
+        text := buffer.String()
+        t.Logf("Gennerated ninja build file:\n%s", text)
+        if !strings.Contains(text, "out/bin/test-out: ") {
+            t.Errorf("Generated ninja file does not have build of the test module")
+        }
+        if !strings.Contains(text, " test-src.go") {
+            t.Errorf("Generated ninja file does not have source dependency")
+        }
+        if !strings.Contains(text, "build vendor: g.gomodule.vendor | go.mod") {
+            t.Errorf("Generated ninja file does not have vendor build rule")
+        }
+    }
+}
